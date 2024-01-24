30837acb14fb0f1b9635044da739070092f21e70
evelyndrake <evelyndrake90@gmail.com>
2024-01-24 17:59:23 -0500

    Fixed authentication

--- index.js
+++ index.js
@@ -133,11 +133,24 @@ app.post('/', async (req, res) => {
 });
 
 app.get('/history', async (req, res) => {
-    try {
-        const entries = await Entry.find({});
-        res.render('history.ejs', { entries });
-    } catch (err) {
-        console.error(err);
-        res.status(500).send('An error occurred while retrieving entries');
+    const auth = {login: 'evelyn', password: process.env.PASSWORD};
+    // parse login and password from headers
+    const b64auth = (req.headers.authorization || '').split(' ')[1] || '';
+    const [login, password] = Buffer.from(b64auth, 'base64').toString().split(':');
+    // Verify login and password are set and correct
+    if (login && password && login === auth.login && password === auth.password) {
+        try {
+            const entries = await Entry.find({});
+            res.render('history.ejs', { entries });
+        } catch (err) {
+            console.error(err);
+            res.status(500).send('An error occurred while retrieving entries');
+        }
+    } else {
+        // Access denied...
+        res.set('WWW-Authenticate', 'Basic realm="401"'); // change this
+        res.status(401).send('Authentication required.'); // custom message
     }
+        
+    
 });
